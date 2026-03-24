ba5709c3b03437c4cc2ede205b9982b0a0d83684
julian <jicobret@proton.me>
2026-03-24 13:52:21 +0100

    add Artifact.ts, create repositories

---
+++ src/models/Artifact.ts
@@ -0,0 +1,22 @@
+import mongoose, { Schema, Document, Types } from "mongoose";
+
+export interface IArtifact extends Document {
+    name: string;
+    description: string;
+    owner: Types.ObjectId;
+}
+
+const ArtifactSchema = new Schema<IArtifact>({
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: "Character",
+        required: true,
+    },
+});
+
+export const ArtifactModel = mongoose.model<IArtifact>(
+    "Artifact",
+    ArtifactSchema
+);
