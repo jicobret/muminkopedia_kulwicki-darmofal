d307cc6bda9e71be27d5431cdb025260e7b39eab
julian <jicobret@proton.me>
2026-03-24 13:50:03 +0100

    install mongoose, add Character.ts

---
+++ src/models/Character.ts
@@ -0,0 +1,28 @@
+import mongoose, { Schema, Document, Types } from "mongoose";
+
+export interface ICharacter extends Document {
+    name: string;
+    description: string;
+    species: string;
+    isHibernating: boolean;
+    bestFriend?: Types.ObjectId;
+}
+
+const CharacterSchema = new Schema<ICharacter>({
+    name: {
+        type: String,
+        required: true,
+    },
+    description: { type: String, required: true },
+    species: { type: String, required: true },
+    isHibernating: { type: Boolean, default: false },
+    bestFriend: {
+        type: Schema.Types.ObjectId,
+        ref: "Character",
+    },
+});
+
+export const CharacterModel = mongoose.model<ICharacter>(
+    "Character",
+    CharacterSchema
+);
